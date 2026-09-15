3800b9e7a2b944c2419f62c2759792c62c1b55f8
SantiCode41 <104732441+SantiCode41@users.noreply.github.com>
2026-09-15 13:21:34 -0500

    Add Helpers class

---
+++ Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinAppCsharp
+{
+    internal class Helpers
+    {
+        public static string GetString()
+        {
+            string returnString = string.Empty;
+            try
+            {
+                returnString = Console.ReadLine();
+                if (string.IsNullOrEmpty(returnString))
+                {
+                    Console.WriteLine("Error at GetString.");
+                    Environment.Exit(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+                Environment.Exit(1);
+            }
+            return returnString;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using FinAppCsharp;
+using Microsoft.Data.Sqlite;
 
 using var db = new Database();
 StartUpSequence.Run(db);
@@ -6,19 +7,51 @@ StartUpSequence.Run(db);
 Console.WriteLine("WELCOME TO DOLLARULE!!!");
 Console.Write("Username: ");
 string userName = string.Empty;
-try
-{
-    userName = Console.ReadLine();
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Something went wrong: {ex.Message}");
-    Environment.Exit(1);
-}
+userName = Helpers.GetString();
 
 db.InsertStartUpLog(userName);
 
 Console.WriteLine($"Welcome {userName}");
+Thread.Sleep(3000);
+
+string menuText = """
+
+    1 - View Budget
+    2 - Create Budget
+    Make Selection: 
+    """;
+Console.Write(menuText);
+string menuChoice = Helpers.GetString();
+int menuChoiceInt = 0;
+if (int.TryParse(menuChoice, out int n))
+{
+    menuChoiceInt = n;
+}
+else
+{
+    Console.WriteLine("Not a valid selection.");
+    Thread.Sleep(2000);
+    Environment.Exit(1);
+}
+
+if (menuChoiceInt == 0)
+{
+    Console.WriteLine("Not a valid selection.");
+    Thread.Sleep(2000);
+    Environment.Exit(1);
+}
+else if (menuChoiceInt == 1)
+{
+    // Run the ViewBudget class
+}
+else if (menuChoiceInt == 2)
+{
+    // Run the CreateBudget class
+}
+else
+{
+    Environment.Exit(2);
+}
 Console.ReadLine();
 Console.Clear();
 Console.WriteLine("Screen should be clear now");
