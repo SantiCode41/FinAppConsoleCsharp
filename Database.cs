065ade7723a91bbe358ec03508a094da8290b77e
SantiCode41 <104732441+SantiCode41@users.noreply.github.com>
2026-09-15 09:25:05 -0500

    Initial commit

---
+++ Database.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.Sqlite;
+
+class Database : IDisposable
+{
+    private readonly SqliteConnection _connection;
+    public Database()
+    {
+        _connection = new SqliteConnection("Data Source=app.db");
+        _connection.Open();
+    }
+
+    public void CreateTableUsers()
+    {
+        var create = _connection.CreateCommand();
+        create.CommandText = """
+            CREATE TABLE IF NOT EXISTS Users (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                FirstName TEXT NOT NULL,
+                LastName TEXT NOT NULL
+            );
+            """;
+        create.ExecuteNonQuery();
+    }
+
+    public void CreateTableLogs()
+    {
+        var create = _connection.CreateCommand();
+        create.CommandText = """
+            CREATE TABLE IF NOT EXISTS Logs (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                User TEXT,
+                Type TEXT NOT NULL,
+                DateTime TEXT NOT NULL
+            );
+            """;
+        create.ExecuteNonQuery();
+    }
+
+    public void InsertStartUpLog(string userName)
+    {
+        var insert = _connection.CreateCommand();
+        string logType = "start up";
+        DateTime dateAndTime = DateTime.Now;
+
+        insert.CommandText = """
+            INSERT INTO Logs (User, Type, DateTime)
+            VALUES ($userName, $logType, $dateAndTime)
+            """;
+        insert.Parameters.AddWithValue("$userName", userName);
+        insert.Parameters.AddWithValue("$logType", logType);
+        insert.Parameters.AddWithValue("$dateAndTime", dateAndTime.ToString("g"));
+
+        insert.ExecuteNonQuery();
+    }
+
+    public void Dispose()
+    {
+        _connection.Dispose();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+using var db = new Database();
+db.CreateTableUsers();
+db.CreateTableLogs();
+
+Console.WriteLine("WELCOME TO DOLLARULE!!!");
+
+Console.Write("Username: ");
+string userName = Console.ReadLine();
+db.InsertStartUpLog(userName);
+
+Console.WriteLine($"Welcome {userName}");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Screen should be clear now");
+
+
+
+
